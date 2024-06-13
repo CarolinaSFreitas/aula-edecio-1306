216a9b2755fba153a72181fd53eaf619fda7e8e4
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-06-13 20:17:46 -0300

    add

---
+++ back/routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express"
+import { filmeCreate, filmeDelete, filmeIndex, filmePesquisa, filmeShow, filmeUpdate } from "./controllers/filmeController.js"
+import { clienteCreate, clienteIndex } from "./controllers/clienteController.js"
+import { loginCliente } from "./controllers/loginController.js"
+import { avaliacaoCreate, avaliacaoDestroy, avaliacaoFilme, avaliacaoIndex } from "./controllers/avaliacaoController.js"
+import { loginAdmin } from "./controllers/adminLoginController.js"
+import { adminCreate, adminIndex } from "./controllers/adminController.js"
+import { verificaToken } from "./middlewares/verificaToken.js"
+
+const router = Router()
+
+router.get("/filmes", filmeIndex)
+      .post("/filmes", verificaToken, filmeCreate)
+      .put("/filmes/:id", verificaToken, filmeUpdate)
+      .delete("/filmes/:id", verificaToken, filmeDelete)
+      .get("/filmes/:id", filmeShow)
+      .get("/filmes/pesquisa/:palavra", filmePesquisa)
+
+router.get("/clientes", clienteIndex)
+      .post("/clientes", clienteCreate)
+      
+router.post("/login", loginCliente)
+
+router.get('/avaliacoes', avaliacaoIndex)
+      .post('/avaliacoes', avaliacaoCreate)
+      .delete('/avaliacoes/:id', avaliacaoDestroy)
+      .get('/avaliacoes/filme/:id', avaliacaoFilme)
+
+router.get('/admin', adminIndex)
+      .post('/admin', adminCreate)
+
+router.post("/loginAdmin", loginAdmin)      
+
+export default router
